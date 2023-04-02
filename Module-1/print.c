5f26a6611d4b75d353860dcdb69f1ac57e961e5e
unknown <joybarua6697@gmail.com>
2023-04-02 18:46:05 +0600

    module-1

---
+++ Module-1/print.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hello World\ti am a programmer\tata kintu new line");
+    return 0;
+}
+// \n
+// \t - tab
+// Hello world 
+// Now i am a programmer
---
+++ Module-1/variable.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int rahim = 10;
+    float karim = 10000.52;
+    char habib = 'z';
+    printf("%0.2f", karim);
+    return 0;
+}
